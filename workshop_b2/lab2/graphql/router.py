36b32d612d067459abb2136e9c84ea346089906c
Mikhail Yohman <mikhail.yohman@gmail.com>
2024-11-15 08:41:10 -0700

    Dev container working and forwarding necessary ports. Initial GraphQL endpoints for devices and sites.

--- workshop_b2/lab2/graphql/router.py
+++ workshop_b2/lab2/graphql/router.py
@@ -1,12 +1,13 @@
 import strawberry
+from neo4j import Driver
 from typing import Annotated
 from fastapi import Depends
 from strawberry.fastapi import GraphQLRouter
-from ..database import get_session, Session
+from ..database import get_db
 from .schema import Query
 
 
-def get_context(session: Annotated[Session, Depends(get_session)]) -> dict:
+def get_context(session: Annotated[Driver, Depends(get_db)]) -> dict:
     return {"session": session}
 
 
